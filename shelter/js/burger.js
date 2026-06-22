4da2b6801ee0842cf75b1eb94e0c9fd20edc2213
MaxPalianski <m.polyansky86@gmail.com>
2026-06-23 00:23:59 +0200

    feat:fix mobile menu styles for pets page

--- shelter/js/burger.js
+++ shelter/js/burger.js
@@ -3,11 +3,13 @@ export function initBurger() {
     const menu = document.querySelector('.header-nav');
     const menuLinks = document.querySelectorAll('.header-nav a');
     const overlay = document.querySelector('.overlay');
+    const logo = document.querySelector('.logo');
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
             burger.classList.remove('open');
             menu.classList.remove('open');
             overlay.classList.remove('open');
+            logo.classList.remove('open');
             document.body.classList.remove('no-scroll');
         });
     });
@@ -15,6 +17,7 @@ export function initBurger() {
         burger.classList.toggle('open');
         menu.classList.toggle('open');
         overlay.classList.toggle('open');
+        logo.classList.toggle('open');
         document.body.classList.toggle('no-scroll');
         console.log('Click');
     });
@@ -22,6 +25,7 @@ export function initBurger() {
         overlay.classList.remove('open');
         burger.classList.remove('open');
         menu.classList.remove('open');
+        logo.classList.remove('open');
         document.body.classList.remove('no-scroll');
     });
 }
