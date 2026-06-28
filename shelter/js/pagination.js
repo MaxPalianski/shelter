7376a6bb56e6d69147957b21e06731156be07d9a
MaxPalianski <m.polyansky86@gmail.com>
2026-06-28 18:13:34 +0200

    feat: complete pagination functionality

--- shelter/js/pagination.js
+++ shelter/js/pagination.js
@@ -21,6 +21,28 @@ function renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage) {
         </div>
         `;
     });
+
+    const pageNumberDisplay = document.querySelector('.pagination-current');
+    if (pageNumberDisplay) pageNumberDisplay.innerText = currentPage;
+    const btnDoubleLeft = document.querySelector('.pagination-double-left');
+    const btnLeft = document.querySelector('.pagination-left');
+    const btnRight = document.querySelector('.pagination-right');
+    const btnDoubleRight = document.querySelector('.pagination-double-right');
+    let maxPage = fullPetsList.length / itemsPerPage;
+    if (currentPage === 1) {
+        btnDoubleLeft.disabled = true;
+        btnLeft.disabled = true;
+    } else {
+        btnDoubleLeft.disabled = false;
+        btnLeft.disabled = false;
+    }
+    if (currentPage === maxPage) {
+        btnRight.disabled = true;
+        btnDoubleRight.disabled = true;
+    } else {
+        btnRight.disabled = false;
+        btnDoubleRight.disabled = false;
+    }
 }
 export async function initPagination() {
     let cardsContainer = document.querySelector('.pets-cards');
@@ -38,4 +60,33 @@ export async function initPagination() {
         itemsPerPage = 4;
     }
     renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage);
+
+    let maxPage = fullPetsList.length / itemsPerPage;
+    const btnRight = document.querySelector('.pagination-right');
+    btnRight.addEventListener('click', () => {
+        if (currentPage < maxPage) {
+            currentPage++;
+            renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage);
+        }
+    });
+    const btnLeft = document.querySelector('.pagination-left');
+    btnLeft.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage);
+        }
+    });
+    const btnDoubleLeft = document.querySelector('.pagination-double-left');
+    btnDoubleLeft.addEventListener('click', () => {
+        currentPage = 1;
+        renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage);
+
+    });
+
+    const btnDoubleRight = document.querySelector('.pagination-double-right');
+    btnDoubleRight.addEventListener('click', () => {
+        currentPage = maxPage;
+        renderPage(cardsContainer, fullPetsList, currentPage, itemsPerPage);
+    });
+
 }
